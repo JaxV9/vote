8609b36ca903d4ea44bbb70030d88b2ee744e7e1
JaxV9 <layanj.pro@gmail.com>
2025-09-17 16:21:03 +0200

    up

--- frontend/src/app/services/http/http.service.ts
+++ frontend/src/app/services/http/http.service.ts
@@ -3,7 +3,7 @@ import { QuickHttp } from '@jaslay/http';
 
 @Injectable()
 export class HttpService {
-  private baseUrl = 'http://localhost:7071/';
+  private baseUrl = 'https://votefunctions.azurewebsites.net/';
   private headers = {
     'Content-Type': 'application/json',
   };
